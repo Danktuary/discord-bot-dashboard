10f4fb6587381d93bbc5088f947790aeef614555
Sanctuary <sanc.pw@gmail.com>
2019-02-27 20:06:29 -0500

    Add webpack aliases

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,14 @@
+const path = require('path');
+
 module.exports = {
 	lintOnSave: false,
+	configureWebpack: {
+		resolve: {
+			alias: {
+				'@': path.join(__dirname, 'src'),
+				'@c': path.join(__dirname, 'src', 'components'),
+				'@v': path.join(__dirname, 'src', 'views'),
+			},
+		},
+	},
 };
