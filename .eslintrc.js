dd7542e194f84543fd56a3fed14ffd3c31fc6290
Sanctuary <sanc.pw@gmail.com>
2019-02-27 19:43:19 -0500

    Update eslint config and lint all files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,33 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
-}
+	extends: 'aqua/vue',
+	env: {
+		node: true,
+	},
+	rules: {
+		'arrow-body-style': 'off',
+		'comma-dangle': ['error', 'always-multiline'],
+		radix: ['error', 'as-needed'],
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-negated-condition': 'off',
+		'operator-linebreak': ['error', 'before'],
+		'quote-props': ['error', 'as-needed'],
+		'vue/max-attributes-per-line': ['error', {
+			singleline: 4,
+			multiline: {
+				max: 1,
+				allowFirstline: false,
+			},
+		}],
+		'vue/html-self-closing': ['error', {
+			html: {
+				void: 'always',
+				normal: 'never',
+				component: 'always',
+			},
+		}],
+		'vue/require-default-prop': 'off',
+		'vue/require-prop-types': 'off',
+		'vue/singleline-html-element-content-newline': 'off',
+	},
+};
--- src/store.js
+++ src/store.js
@@ -1,16 +1,16 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
+	state: {
 
-  },
-  mutations: {
+	},
+	mutations: {
 
-  },
-  actions: {
+	},
+	actions: {
 
-  }
-})
+	},
+});
