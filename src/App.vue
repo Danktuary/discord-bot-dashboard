765537c7080dcbb7a9ad73642bbb6c5dd5a32d11
Sanctuary <sanc.pw@gmail.com>
2019-02-27 23:58:56 -0500

    Remove boilerplate css, add custom scss file

--- src/App.vue
+++ src/App.vue
@@ -23,23 +23,6 @@ export default {
 };
 </script>
 
-
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import 'assets/sass/app.scss';
 </style>
