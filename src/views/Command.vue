9bfb201795eee3c95cd59d2104b443f381def112
Sanctuary <sanc.pw@gmail.com>
2019-03-02 22:46:47 -0500

    Update Command view, add command examples

--- src/views/Command.vue
+++ src/views/Command.vue
@@ -1,18 +1,32 @@
 <template>
 	<transition name="fade" mode="out-in">
 		<div id="command" :key="$route.params.command">
-			<b-card :header="command.name" class="bg-dark">
-				{{ command.description }}
+			<b-card
+				class="bg-dark"
+			>
+				<template slot="header">
+					Command info about <strong>{{ command.displayName }}</strong>
+				</template>
+				<b-card-sub-title>{{ command.description }}</b-card-sub-title>
+				<div v-if="command.examples && command.examples.length">
+					<command-example
+						v-for="(example, index) in command.examples"
+						:key="`example-${index}`"
+						:example="example"
+					/>
+				</div>
 			</b-card>
 		</div>
 	</transition>
 </template>
 
 <script>
+import CommandExample from './CommandExample.vue';
 import commands from '@/data/commands.js';
 
 export default {
 	name: 'Command',
+	components: { CommandExample },
 	computed: {
 		command() {
 			return commands.find(cmd => cmd.name === this.$route.params.command);
---
+++ src/views/CommandExample.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		Input: {{ example.input }}
+		<br />
+		Output: {{ example.output }}
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CommandExample',
+	props: { example: Object },
+};
+</script>
